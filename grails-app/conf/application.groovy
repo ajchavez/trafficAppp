b6c73fff985753197909b33ad3f0ec922b7979f9
bmwichma <bmwichma@mtu.edu>
2020-03-24 17:10:19 -0400

    Tomcats server coding completed

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,45 @@
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'trafficapp.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'trafficapp.UserRole'
+grails.plugin.springsecurity.authority.className = 'trafficapp.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+	[pattern: '',                access: ['permitAll']],
+	[pattern: '/**/node/**',     access: ['permitAll']],
+	[pattern: '**/home/**',        access: ['permitAll']],
+	[pattern: '**/leaflet.js/**',  access: ['permitAll']],
+	[pattern: '**/studentView.css/**',  access: ['permitAll']],
+	[pattern: '**/studentLeaflet.js/**', access: ['permitAll']],
+
+	//for admin access to dbconsole
+	[pattern: '/**/dbconsole/**',   access: ['ROLE_ADMIN']],
+]
+
+grails.plugin.springsecurity.filterChain.chainMap = [
+	[pattern: '/assets/**',      filters: 'none'],
+	[pattern: '/**/js/**',       filters: 'none'],
+	[pattern: '/**/css/**',      filters: 'none'],
+	[pattern: '/**/images/**',   filters: 'none'],
+	[pattern: '/**/favicon.ico', filters: 'none'],
+	[pattern: '/**',             filters: 'JOINED_FILTERS']
+]
+
+environments {
+	production {
+		grails.dbconsole.enabled = true
+		grails.dbconsole.urlRoot = '/dbconsole'
+	}
+}
+
+
